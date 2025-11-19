e5d47e16bc00941a0af8a3cbea364170167b9d8e
Retriever5500 <Kilowatt7028@tutanota.com>
2025-11-19 22:36:20 +0330

    Fix env's termination in test

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ env = ShoverWorldEnv(render_mode=None, n_rows=6, n_cols=9, number_of_boxes=15,
     number_of_barriers=5, number_of_lavas=4, initial_force=4.0, unit_force=1.0)
 
 obs, info = env.reset()
-done = False
+done, trun = False, False
 total_r = 0.0
 
-while not done:
+while not done and not trun:
     a = env.action_space.sample()
     obs, r, done, trun, info = env.step(a)
     total_r += r
